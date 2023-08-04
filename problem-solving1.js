e52130d3b0072e8d03a666f127bafaae8133ceb6
safinhr <hrsafin2434@gmail.com>
2023-08-04 16:44:11 +0600

    more problem solved

--- problem-solving1.js
+++ problem-solving1.js
@@ -14,3 +14,53 @@ function sumOfArray(array){
 const arrayOfNumber = [1,3,5,3,52]
 const sumResult = sumOfArray(arrayOfNumber)
 console.log(sumResult)
+
+//2. sum of all the array element(for of loop)
+
+function sum(arr){
+   let sum = 0
+   for(let i of arr){
+    sum = sum + i
+   }
+
+   return sum
+}
+
+const nums = [1,2,3,4,5]
+
+const sumArray = sum(nums)
+console.log(sumArray)
+
+//3. sum of all odd number from an array
+
+function oddSum(array){
+    let sumOfOdd =0
+    for(let i of array){
+        if(i%2 !==0){
+            sumOfOdd += i
+        }
+    }
+
+    return sumOfOdd
+}
+
+const oddArray = [1,3,4,54,31,73,84]
+const resultOdd = oddSum(oddArray)
+console.log("Sum of Odd number:",resultOdd)
+
+//4. sum of all the even number from an array
+
+function evenSum(array){
+    let sumOfEven =0
+    for(let i of array){
+        if(i%2 ===0){
+            sumOfEven += i
+        }
+    }
+
+    return sumOfEven
+}
+
+const evenArray = [1,3,4,54,31,73,84]
+const resultEven = evenSum(evenArray)
+console.log("Sum of Even number:",resultEven)
