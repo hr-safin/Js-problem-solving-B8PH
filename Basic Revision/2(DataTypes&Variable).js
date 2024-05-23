683f933582cf70c743c2a859003ece5dd40f2d81
safinhr <hrsafin2434@gmail.com>
2024-05-23 14:24:26 +0600

    variable in js

---
+++ Basic Revision/2(DataTypes&Variable).js
@@ -0,0 +1,11 @@
+// Data Types --> Undefined, null , boolean , string, number, symbol and object
+
+var myName = "Hasibur Rahman Safin"
+
+myName = "Nur-Al Munira"
+
+let myId = 21301585
+
+myId = "Insha Allah Njit Id"
+
+const univerName = "Insha Allah New jersey institute of bangladesh will my university"
