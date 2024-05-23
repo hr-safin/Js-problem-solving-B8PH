d93b477e58793e61549e9c2391a1ca02e8d8ef36
safinhr <hrsafin2434@gmail.com>
2024-05-23 14:33:27 +0600

    initialized variable

--- Basic Revision/2(DataTypes&Variable).js
+++ Basic Revision/2(DataTypes&Variable).js
@@ -9,3 +9,32 @@ let myId = 21301585
 myId = "Insha Allah Njit Id"
 
 const univerName = "Insha Allah New jersey institute of bangladesh will my university"
+
+
+// How to declare and assign variable
+
+//var a; // Declaring the variable
+//var b = 6  // Declaring and assigning the variable in the same time
+
+// console.log(a) // undefined
+//a = 8
+//b =9
+
+// console.log(a)
+// console.log(b)
+
+
+// Initialize these three variable 
+
+var a = 5;
+var b = 5;
+var c = "I am a";
+
+
+a = a + 1
+b = b + 5
+c = c + " string!"
+
+console.log(a)
+console.log(b)
+console.log(c)
