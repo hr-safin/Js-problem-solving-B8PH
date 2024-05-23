486ac3173b528b98e62fe877c8bc03030e763204
safinhr <hrsafin2434@gmail.com>
2024-05-23 14:39:42 +0600

    case sensitive : camelCase

---
+++ Basic Revision/3(Case Sensitive).js
@@ -0,0 +1,13 @@
+// camelCase
+
+// Declaration 
+var studlyCapVar ;
+var myName ;
+
+// Assignments
+
+studlyCapVar = 10;
+myName = 'Hasibur Rahman Safin'
+
+console.log(studlyCapVar)
+console.log(myName)
